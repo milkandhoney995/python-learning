7ae8f65f88f83e75ac792a15a51b55c969a38954
cri-harada.utano <harada.utano@cri-mw.co.jp>
2023-01-24 10:47:46 +0900

    書籍「テスト駆動開発」1,2章完了

--- test.py
+++ test.py
@@ -1,2 +1,19 @@
 import unittest
 
+# テストコード(Dollarが正しく動くかを確認する)
+class MoneyTest(unittest.TestCase):
+  def testMultiplication(self):
+    five = Dollar(5)
+    product = five.times(2)
+    self.assertEqual(10, product.amount)
+
+# 実装コード
+class Dollar():
+
+  def __init__(self, amount):
+    self.amount = amount
+  def times(self, multiplier):
+    return Dollar(self.amount * multiplier)
+# テストの実行方法
+if __name__ == '__main__':
+  unittest.main()
